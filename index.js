ea1025968b6b7f0acdc3886673d6c7b25803d685
mehmud <timehmud121@gmail.com>
2023-10-18 18:58:40 +0600

    added post method and get method

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tf63vau.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,6 +24,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db("productDB").collection("product");
+
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
